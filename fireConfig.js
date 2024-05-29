d82db5eb9145479a0d6ff3ff8fd71a5d97a739fb
JamesBueno08 <144246056+JamesBueno08@users.noreply.github.com>
2024-05-29 14:46:46 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBoOeL5hUpJArZw-0hs7axU2KY1YCzLqbo",
+  authDomain: "crudfinal-2e8cf.firebaseapp.com",
+  databaseURL: "https://crudfinal-2e8cf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crudfinal-2e8cf",
+  storageBucket: "crudfinal-2e8cf.appspot.com",
+  messagingSenderId: "598774549468",
+  appId: "1:598774549468:web:17b148ed5d2142d5889d19"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
